6ac01d0ebe4ce8e76122dc7197cf1aebbd730dfb
Vic Chimenti <chimenti@seattleu.edu>
2018-11-27 10:53:06 -0800

    Update dht_client.py

--- dht_client.py
+++ dht_client.py
@@ -8,4 +8,14 @@
 
 #   Distributed Hash Table Client
 
+
+
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 10109
+MESSAGE = "Hello World"
+sock = socket.socket(socket.AF_INET, socket.SOCK_DRGAM)
+sock.sento(MESSAGE, (UDP_IP, UDP_PORT))
+
+
 #   eof
