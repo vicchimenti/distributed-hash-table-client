51d173efa37fca07ed65d21a526e2cf128aa3d38
Vic Chimenti <chimenti@seattleu.edu>
2018-11-29 22:36:09 -0800

    Update dht_client.py

--- dht_client.py
+++ dht_client.py
@@ -54,7 +54,7 @@ def getIP(h) :
 
 # DEFINE CONTSANTS
 MATCH_ALL = "0.0.0.0"       # for IP validity checking
-MY_PORT = 10101             # pre-defined client port number
+MY_PORT = 10103             # pre-defined client port number
 
 # define defaults
 charset = "UTF-8"           # default encoding protocol
