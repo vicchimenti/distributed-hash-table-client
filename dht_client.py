bd1309687918d3699fd48b84f77d11c4b9bf2ee0
Vic Chimenti <chimenti@seattleu.edu>
2018-12-04 21:12:27 -0800

    Update dht_client.py

--- dht_client.py
+++ dht_client.py
@@ -68,7 +68,7 @@ def validateOperator(op, v) :
 
 # DEFINE CONTSANTS
 MATCH_ALL = "0.0.0.0"       # for IP validity checking
-MY_PORT = 10105             # pre-defined client port number
+MY_PORT = 10119             # pre-defined client port number
 NEWLINE = '\n'              # newline constant
 GET = 'get'                 # get operator
 PUT = 'put'                 # put operator
