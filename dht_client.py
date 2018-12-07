32ba3a9297f409b8972fef300f1b942f9f4ae88c
Vic Chimenti <chimenti@seattleu.edu>
2018-12-06 17:21:25 -0800

    Update dht_client.py

--- dht_client.py
+++ dht_client.py
@@ -3,7 +3,7 @@
 #   p2 Distributed Hash Table
 #   dht_client.py
 #   created         11/26/2018
-#   last modified   12/5/2018
+#   last modified   12/6/2018
 #   Distributed Hash Table Client
 #   /usr/local/python3/bin/python3
 
